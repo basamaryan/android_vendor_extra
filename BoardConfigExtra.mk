9948aa3a979c7b3aa092be4563c22cd2f045de3e
basamaryan <basam.aryan@gmail.com>
2024-10-29 21:23:05 -0400

    Clean-up for my use
    Change-Id: I2b108b20f81e60e2c6ffae523a71a17b2ab9289c

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,14 +1,11 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # Google Sans
 -include vendor/google_sans/board.mk
-
-# MiuiCamera
--include device/xiaomi/miuicamera-$(PRODUCT_DEVICE)/BoardConfig.mk
 
 # Pixel Clocks
 -include vendor/pixel_clocks/board.mk
--- product.mk
+++ product.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,15 +7,5 @@
 # Google Sans
 $(call inherit-product-if-exists, vendor/google_sans/product.mk)
 
-# MiuiCamera
-$(call inherit-product-if-exists, device/xiaomi/miuicamera-$(shell echo -n $(TARGET_PRODUCT) | sed -e 's/^[a-z]*_//g')/device.mk)
-
-# OTA
-PRODUCT_PACKAGES += UpdaterOverlay
-
 # Pixel Clocks
 $(call inherit-product-if-exists, vendor/pixel_clocks/product.mk)
-
-# Translations
-PRODUCT_PACKAGES += \
-    MotorTranslationsOverlay

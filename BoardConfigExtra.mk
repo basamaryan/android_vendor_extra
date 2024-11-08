36dde60bc3b87beaa7e712f52d1517c89600cddb
basamaryan <basam.aryan@gmail.com>
2024-11-08 15:33:38 -0500

    Guard with WITH_GMS
    Change-Id: I9f318b64152e670aa9f92ce17c66510c303df9b2

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -4,8 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+ifeq ($(WITH_GMS),true)
 # Google Sans
 -include vendor/google_sans/board.mk
 
 # Pixel Clocks
 -include vendor/pixel_clocks/board.mk
+endif
--- product.mk
+++ product.mk
@@ -4,8 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+ifeq ($(WITH_GMS),true)
 # Google Sans
 $(call inherit-product-if-exists, vendor/google_sans/product.mk)
 
 # Pixel Clocks
 $(call inherit-product-if-exists, vendor/pixel_clocks/product.mk)
+endif

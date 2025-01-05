282ffacf08abc9d1466c96c1d5049ee75f90a8bb
AdarshGrewal <adarshgrewal@gmail.com>
2025-01-05 14:54:12 +0530

    extra: Add function for applying patches
    Change-Id: I4d332373dcd9a896ddcb29b63935a1d7e69bad0e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -112,6 +112,36 @@ generate_changelog() {
     done
 }
 
+apply_patches() {
+    local top="${ANDROID_BUILD_TOP:-$PWD}"
+    local patches_path="${top}/vendor/extra/patches"
+
+    cd "${top}" || return 1
+
+    #./vendor/lineage/build/tools/repopick.py -t md3e-flags
+
+    if [[ ! -d "${patches_path}" ]]; then
+        echo "[INFO] No patches directory found, skipping local patches."
+        return 0
+    fi
+
+    for project_name in $(cd "${patches_path}"; echo */); do
+        [[ "${project_name}" == "*/" ]] && continue
+        local project_path="$(tr _ / <<<"${project_name}")"
+        project_path="${project_path%/}"
+
+        cd "${top}/${project_path}" || continue
+
+        echo "Applying patches for project: ${project_name}"
+        if ! git am "${patches_path}/${project_name}"*.patch --no-gpg-sign; then
+            echo "Failed to apply patches for project: ${project_name}. Aborting."
+            git am --abort &>/dev/null
+        fi
+
+        cd "${top}"
+    done
+}
+
 function release() {
     local devices=()
     local skip_sync=false
@@ -155,8 +185,9 @@ function release() {
         repo sync --force-sync -d -j"${JOBS}" || return 1
     fi
 
-    if [[ "${skip_picks}" == "false" && -x "./picks" ]]; then
-        ./picks || return 1
+    if [[ "${skip_picks}" == "false" ]]; then
+        apply_patches
+        [[ -x "./picks" ]] && ./picks || return 1
     fi
 
     for device in "${devices[@]}"; do

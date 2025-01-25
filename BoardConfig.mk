49cc0e02d4ca6e712ab9578fe285e5bdc38acddb
basamaryan <basam.aryan@gmail.com>
2025-01-25 09:32:59 -0500

    Reserve 50MB on /system
    Change-Id: Ib7dc535dff76d35a4e052c2940e1bf8f704aaa17

---
+++ BoardConfig.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq ($(WITH_GMS),true)
+    BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 52428800
+endif

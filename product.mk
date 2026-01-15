19cf8aeb934f18b83167b57cc982f29d2f3d4484
basamaryan <basam.aryan@gmail.com>
2026-01-14 18:26:29 -0800

    Revert "[TMP] Revert "Build oplus camera""
    This reverts commit df2027947ffd646c2329cb2c9b24639b13138df9.

--- product.mk
+++ product.mk
@@ -7,6 +7,11 @@
 # MiuiCamera
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-$(shell echo -n $(TARGET_PRODUCT) | sed -e 's/^[a-z]*_//g')/device.mk)
 
+# OplusCamera
+ifeq ($(TARGET_PRODUCT),lineage_martini)
+  $(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+endif
+
 ifeq ($(WITH_GMS),true)
 # OTA
 PRODUCT_PACKAGES += \

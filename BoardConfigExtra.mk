ccb328f675bf5798384907c4c8a1be6fd6e8b2b3
basamaryan <basam.aryan@gmail.com>
2026-03-07 15:57:29 -0800

    vendorsetup: Update MiuiCamera path
    Change-Id: Iaaa966851e52309b407fe7c413582dd1ef3fb205

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,10 +1,10 @@
 #
-# Copyright (C) 2022-2025 The LineageOS Project
+# Copyright (C) 2022-2026 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
--include device/xiaomi/miuicamera-$(PRODUCT_DEVICE)/BoardConfig.mk
+-include vendor/miuicamera-$(PRODUCT_DEVICE)/BoardConfig.mk
 
 ifeq ($(WITH_GMS),true)
 # Pixel Goodies
--- product.mk
+++ product.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2022-2025 The LineageOS Project
+# Copyright (C) 2022-2026 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 # MiuiCamera
-$(call inherit-product-if-exists, device/xiaomi/miuicamera-$(shell echo -n $(TARGET_PRODUCT) | sed -e 's/^[a-z]*_//g')/device.mk)
+$(call inherit-product-if-exists, vendor/miuicamera-$(shell echo -n $(TARGET_PRODUCT) | sed -e 's/^[a-z]*_//g')/device.mk)
 
 # OplusCamera
 ifeq ($(TARGET_PRODUCT),lineage_martini)
